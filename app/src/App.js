0b5b95f38c537291c7839e6e9bec3bd94f36e9f0
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-07-28 13:40:30 -0400

    Implemented basic network requests

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
 import WeatherCard from './components/WeatherCard';
+import secretKeys from './secret';
 
-const cards = [32256];
+const Stack = styled.div`
+	display: flex;
+	padding: 1em;
 
-function App() {
-	return (
-		<div className="dashboard">
-			{cards.map((zip, i) => {
-				return <WeatherCard zipCode={zip} key={i} />;
-			})}
-		</div>
-	);
+	${(props) =>
+		css`
+			flex-direction: ${props.vertical ? 'column' : 'row'};
+		`};
+`;
+
+const Button = styled.button`
+	color: black;
+`;
+
+const Title = styled.h1`
+	${(props) =>
+		css`
+			color: ${props.color ? props.color : 'palevioletred'};
+		`};
+`;
+
+class App extends Component {
+	state = {
+		cities: {},
+	};
+
+	url = 'http://api.openweathermap.org/data/2.5/weather?';
+
+	render() {
+		return (
+			<Stack vertical={true}>
+				<Stack>
+					<Button> Add </Button>
+					<Button onClick={() => this.update('32256')}>
+						{' '}
+						Refresh{' '}
+					</Button>
+					<Title color={'red'}> Weather Deck </Title>
+				</Stack>
+				<Stack>
+					{Object.keys(this.state.cities).map((key) => {
+						return (
+							<WeatherCard
+								data={this.state.cities[key]}
+								key={key}
+							/>
+						);
+					})}
+				</Stack>
+			</Stack>
+		);
+	}
+
+	update = async (zip) => {
+		fetch(`${this.url}zip=${zip}&appid=${secretKeys.api_key}`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((res) => {
+				console.log(res);
+				this.setState({ cities: { zip: res } });
+			});
+	};
 }
 
 export default App;
--- app/src/components/WeatherCard.jsx
+++ app/src/components/WeatherCard.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
+
+const Card = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	border-radius: 2em;
+	width: 15em;
+	height: 20em;
+	margin: 1em;
+
+	${(props) =>
+		css`
+			background-color: ${props.color ? props.color : 'papayawhip'};
+		`};
+`;
+
+const Title = styled.h1`
+	${(props) =>
+		css`
+			color: ${props.color ? props.color : 'palevioletred'};
+		`};
+`;
 
 class WeatherCard extends Component {
 	render() {
-		return <div>Card.</div>;
+		return (
+			<Card>
+				<Title>{this.props.data.name}</Title>
+				<Title color={'black'}>
+					{this.kelvinToFahrenheit(this.props.data.main.temp)}°
+				</Title>
+			</Card>
+		);
 	}
+
+	kelvinToFahrenheit = (temp) => {
+		return Math.round(temp * 1.8 - 459.67);
+	};
 }
 
 export default WeatherCard;
